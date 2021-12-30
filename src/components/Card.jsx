8c8f1fbef1baaff8b722030bcc7525de1899240a
Shiyu <shiyu.huang93@gmail.com>
2021-12-30 19:48:33 +0800

    extracted Card wrapper component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.scss"
+
+function Card(props) {
+    // make sure "className" attribute works with custom components
+    const classes = 'card ' + props.className;
+    
+    //value of children will always be content between opening and closing
+    // tags of our custom component
+    return <div className={classes}>{props.children}</div> 
+}
+
+export default Card;
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,17 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.scss"
+import Card from "./Card";
+import "./Expenses.scss";
 
 function Expenses(props) {
-    return (
-        <div className="expenses">
-            {
-                props.expenses.map(function(expense, i) {
-                    return <ExpenseItem key={i} title={expense.title} date={expense.date} amount={expense.amount} />
-                })
-            }
-        </div>
-    )
+  return (
+    <Card className="expenses">
+      {props.expenses.map(function (expense, i) {
+        return (
+          <ExpenseItem
+            key={i}
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        );
+      })}
+    </Card>
+  );
 }
 
 export default Expenses;
-
